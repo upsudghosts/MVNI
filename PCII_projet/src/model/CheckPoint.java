ad8a421625d1a8e725644806ac55e5404bd8a5ad
patricia.ferreira-alves <patricia.ferreira-alves@a-018972.ups.u-psud.fr>
2020-02-26 14:28:54 +0100

    description fonctions checkpoint

--- PCII_projet/src/model/CheckPoint.java
+++ PCII_projet/src/model/CheckPoint.java
@@ -8,10 +8,14 @@ public class CheckPoint {
 		this.height = hor;
 	}
 	
+	/**Lowers the CheckPoint by increasing the value of the height variable**/
 	public void decreaseHeight(int n) {
 		this.height += n;
 	}
 	
+	/**Gives the height of the checkpoint
+	 * @return an int, the height
+	 **/
 	public int getHeight() {
 		return this.height;
 	}
